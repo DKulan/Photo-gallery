10cae1dee527b5f0e2793fe44bc2893c9ed19fa8
Daniel Kulangiev <danny.kulangiev@gmail.com>
2019-07-20 11:17:20 -0400

    Fix query string

--- src/API/index.js
+++ src/API/index.js
@@ -1,8 +1,8 @@
-export const CLIENT_ID = 'KEY GOES HERE!'
+export const CLIENT_ID = 'API KEY GOES HERE!'
 
 
 const loadImages = (searchTerm, orientation, featured) => {
-  return fetch(`https://api.unsplash.com/photos/random?&count=30&client_id=${CLIENT_ID}${searchTerm && `&query=${searchTerm}`}${orientation && `&orientation=${orientation}`}${featured && `&featured=${true}`}`)
+  return fetch(`https://api.unsplash.com/photos/random?&count=30&client_id=${CLIENT_ID}${searchTerm && `&query=${searchTerm}`}${orientation && `&orientation=${orientation}`}${featured ? `&featured=${true}` : ''}`)
     .then((response) => {
       if (response.status === 200) {
         return response.json()
